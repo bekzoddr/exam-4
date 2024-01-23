0be6dd739e5387e55538655bc85df14fded37a6d
bekzoddr <sodiqov.bekzoddr@gmail.com>
2024-01-24 00:21:07 +0500

    some changes

---
+++ js/main.js
@@ -0,0 +1,7 @@
+ const productCard = document.createElement("div");
+ const productCardImg = document.createElement("img");
+ const productCard__Bottom = document.createElement("div");
+ const productCard__BottomPrice = document.createElement("div");
+ const productCard__BottomSell = document.createElement("div");
+
+
